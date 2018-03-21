b9c968d74644f93a8e33280e4b6d1d05dd024133
Aaron Akbari-Mort and Kristen Chellis <kristen@>
2018-03-21 10:27:31 -0700

    add return to form button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-#letter {
-  display: none;
-}
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var nameFirstInput = $("input#nameFirst").val();
+    var nameLastInput = $("input#nameLast").val();
+
+    $(".nameFirst").text(nameFirstInput);
+    $(".nameLast").text(nameLastInput);
+
+    $("#letter").show();
+    $("#formOne").hide();
+    event.preventDefault();
+  });
+  $("#return").click(function(){
+    $("#letter").hide();
+    $("#formOne").show();
+    $("#formOne").trigger("reset");
+  });
+});
